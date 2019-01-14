36bb762906580f993b659fac6ed7198495c06ba5
LiuTianhang <E0175471@u.nus.edu>
2019-01-14 18:15:57 +0800

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# react_carousel
-Created with CodeSandbox
---
+++ src/Carousel.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+
+export default () => (
+  <Carousel autoPlay>
+    <div>
+      <p style={{ color: "white" }}>hello</p>
+    </div>
+    <div>
+      <p style={{ color: "white" }}>hello</p>
+    </div>
+  </Carousel>
+);
