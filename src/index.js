5e4904e218ac53f76a94f170d9c34cd159a27d98
LIU-TIANHANG <you@example.com>
2019-01-14 20:38:00 +0800

    updated carousel

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React ,{Component} from "react";
 import { render } from "react-dom";
 import Carousel from "./Carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-const App = () => (
-  <div>
-    <Carousel />
-  </div>
-);
+class App extends Component{
+    render() {
+        return(
+            <div>
+                <Carousel />
+            </div>
+        )
+    }
+}
 
 render(<App />, document.getElementById("root"));
